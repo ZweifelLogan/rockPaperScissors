2f8f101da9dad852ccd4ceb48c050fc8c60577f2
ZweifelLogan <zweifellogan@gmail.com>
2022-09-21 08:48:45 -0500

    COMPLETED FIRST TOP rock-paper-scissors project

--- rps.js
+++ rps.js
@@ -2,10 +2,83 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     //console.log(choice);
     if (choice == 0) {
-        return "Rock";
+        return "rock";
     } else if (choice == 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            return "tie";
+        } else if (computerSelection === "paper") {
+            return "You lost";
+        } else {
+            return "You won! :)";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "you won! :)";
+        } else if (computerSelection === "paper") {
+            return "tie";
+        } else {
+            return "you lost";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "you lost";
+        } else if (computerSelection === "paper") {
+            return "You won! :)";
+        } else {
+            return "tie";
+        }
+    } else {
+        return "invalid player input. choose rock, paper, or scissors";
+    }
+}
+
+function promptPlayer() {
+    return prompt("Enter your rock-paper-scissors choice").toLowerCase();
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        //console.log(playRound(getComputerChoice(),getComputerChoice()));
+        console.log(playRound(promptPlayer(),getComputerChoice()));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
